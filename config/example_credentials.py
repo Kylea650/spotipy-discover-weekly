3fdb68ff291fdde450c5b4b5dfb1203aa8b40f6f
Kylea650 <90274941+Kylea650@users.noreply.github.com>
2022-08-19 15:02:23 +0100

    adding example credentials file

---
+++ config/example_credentials.py
@@ -0,0 +1,9 @@
+"""
+The CLIENT_ID and CLIENT_SECRET information can be obtained through creating an app linked 
+to your spotify account on https://developer.spotify.com/
+"""
+CLIENT_ID = "<your client id>"
+CLIENT_SECRET = "<your client secret>"
+
+REDIRECT_URI = "<your re-direct URI>"  # usually http://localhost:8080/
+USER_ID = "<your username>"  # can be found through user > account > username
