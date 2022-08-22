75052b001dab35695a02673628ddf355e6b47599
Kylea650 <90274941+Kylea650@users.noreply.github.com>
2022-08-22 10:01:56 +0100

    Updating env variables

--- discover_weekly.py
+++ discover_weekly.py
@@ -4,21 +4,11 @@ from datetime import datetime
 import os
 
 
-try:  # for running locally
-    from config import credentials
-
-    CLIENT_ID = credentials.CLIENT_ID
-    CLIENT_SECRET = credentials.CLIENT_SECRET
-    REDIRECT_URI = credentials.REDIRECT_URI
-    USER_ID = credentials.USER_ID
-    PLAYLIST_ID = credentials.PLAYLIST_ID
-
-except:  # for running with GitHub Actions
-    CLIENT_ID = os.environ["CLIENT_ID"]
-    CLIENT_SECRET = os.environ["CLIENT_SECRET"]
-    REDIRECT_URI = os.environ["REDIRECT_URI"]
-    USER_ID = os.environ["USER_ID"]
-    PLAYLIST_ID = os.environ["PLAYLIST_ID"]
+CLIENT_ID = os.environ["CLIENT_ID"]
+CLIENT_SECRET = os.environ["CLIENT_SECRET"]
+REDIRECT_URI = os.environ["REDIRECT_URI"]
+USER_ID = os.environ["USER_ID"]
+PLAYLIST_ID = os.environ["PLAYLIST_ID"]
 
 
 client = spotipy.Spotify(
