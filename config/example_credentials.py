082f695f072f2b25644536c424c7062a4f7c2036
Kylea650 <90274941+Kylea650@users.noreply.github.com>
2022-08-22 10:39:49 +0100

    Adding more robust authentication

--- config/example_credentials.py
+++ config/example_credentials.py
@@ -4,6 +4,7 @@ to your spotify account on https://developer.spotify.com/
 """
 CLIENT_ID = "<your client id>"
 CLIENT_SECRET = "<your client secret>"
+REFRESH_TOKEN = "<your refresh token from the .cache>"
 
 REDIRECT_URI = "<your re-direct URI>"  # usually http://localhost:8080/
 USER_ID = "<your username>"  # can be found through user > account > username
--- discover_weekly.py
+++ discover_weekly.py
@@ -1,7 +1,10 @@
+from http import client
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from datetime import datetime
 import os
+
+from config.credentials import REFRESH_TOKEN
 
 
 try:  # for running locally
@@ -12,6 +15,7 @@ try:  # for running locally
     REDIRECT_URI = credentials.REDIRECT_URI
     USER_ID = credentials.USER_ID
     PLAYLIST_ID = credentials.PLAYLIST_ID
+    REFRESH_TOKEN = credentials.REFRESH_TOKEN
 
 except:  # for running with GitHub Actions
     CLIENT_ID = os.environ["CLIENT_ID"]
@@ -19,22 +23,28 @@ except:  # for running with GitHub Actions
     REDIRECT_URI = os.environ["REDIRECT_URI"]
     USER_ID = os.environ["USER_ID"]
     PLAYLIST_ID = os.environ["PLAYLIST_ID"]
+    REFRESH_TOKEN = os.environ["REFRESH_TOKEN"]
 
 
-client = spotipy.Spotify(
-    auth_manager=SpotifyOAuth(
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        redirect_uri=REDIRECT_URI,
+def get_client(client_id, client_secret, redirect_uri, refresh_token):
+    """
+    returns the authenticated Spotify client
+    """
+    auth_manager = SpotifyOAuth(
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri=redirect_uri,
         scope=[
             "playlist-read-private",
             "playlist-modify-private",
         ],
     )
-)
+    auth_manager.refresh_access_token(refresh_token=refresh_token)
+    client = spotipy.Spotify(auth_manager=auth_manager)
+    return client
 
 
-def get_discover_weekly_tracks(playlist_id) -> list[str]:
+def get_discover_weekly_tracks(client, playlist_id) -> list[str]:
     """
     returns all track URIs for the current week's Discover Weekly playlist
     """
@@ -42,7 +52,7 @@ def get_discover_weekly_tracks(playlist_id) -> list[str]:
     return [track["track"]["uri"] for track in discover_weekly_tracks["items"]]
 
 
-def get_discover_weekly_date(playlist_id) -> str:
+def get_discover_weekly_date(client, playlist_id) -> str:
     """
     returns the playlist date of the current week's Discover Weekly playlist
     in YYYY-MM-DD format
@@ -54,13 +64,12 @@ def get_discover_weekly_date(playlist_id) -> str:
     return created_date.strftime("%Y-%m-%d")
 
 
-def create_new_playlist(playlist_id) -> dict:
+def create_new_playlist(client, user, playlist_id) -> dict:
     """
     creates a new Spotify playlist for the current Discover Weekly playlist
     to be archived and returns the new playlist info as a dict object
     """
-    user = USER_ID
-    date = get_discover_weekly_date(playlist_id=playlist_id)
+    date = get_discover_weekly_date(client=client, playlist_id=playlist_id)
     new_playlist = client.user_playlist_create(
         user=user,
         name=f"Discover(ed) Weekly {date}",
@@ -70,23 +79,33 @@ def create_new_playlist(playlist_id) -> dict:
     return new_playlist
 
 
-def archive_discover_weekly(playlist_id, new_playlist) -> None:
+def archive_discover_weekly(client, user, playlist_id, new_playlist) -> None:
     """
     adds tracks from current Discover Weekly playlist into
     newly created archived Discover(ed) Weekly playlist
     """
-    user = USER_ID
     client.user_playlist_add_tracks(
         user=user,
         playlist_id=new_playlist["id"],
-        tracks=get_discover_weekly_tracks(playlist_id=playlist_id),
+        tracks=get_discover_weekly_tracks(client=client, playlist_id=playlist_id),
     )
 
 
 def main() -> None:
+    user = USER_ID
     playlist_id = PLAYLIST_ID
-    new_playlist = create_new_playlist(playlist_id=playlist_id)
-    archive_discover_weekly(playlist_id=playlist_id, new_playlist=new_playlist)
+    client = get_client(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=REDIRECT_URI,
+        refresh_token=REFRESH_TOKEN,
+    )
+    new_playlist = create_new_playlist(
+        client=client, user=user, playlist_id=playlist_id
+    )
+    archive_discover_weekly(
+        client=client, user=user, playlist_id=playlist_id, new_playlist=new_playlist
+    )
 
 
 if __name__ == "__main__":
